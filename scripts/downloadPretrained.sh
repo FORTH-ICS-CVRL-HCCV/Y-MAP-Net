6eb6810f87bc05f82baf9393c7b425cd5b86cdb5
Ammar Qammaz <ammarkov@gmail.com>
2026-01-31 23:15:31 +0200

    ...

--- scripts/downloadPretrained.sh
+++ scripts/downloadPretrained.sh
@@ -4,11 +4,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 cd ..
 
-#V180 is the ICRA 26 model!
-DLNAME="2d_pose_estimation_v180.zip"
 
-rm $DLNAME
-wget http://ammar.gr/ymapnet/archive/$DLNAME
-unzip -o $DLNAME
+#V180 is the ICRA 26 model!
+scripts/downloadModel.sh 180
+
+
+
+#DLNAME="2d_pose_estimation_v180.zip"
+
+#rm $DLNAME
+#wget http://ammar.gr/ymapnet/archive/$DLNAME
+#unzip -o $DLNAME
 
 exit 0
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -38,23 +38,21 @@ if [ "$USE_VENV" -eq "0" ]; then
   python3 -m pip install tensorflow tensorflow-model-optimization tf_keras numpy numba tensorboard tensorboard-plugin-profile etils importlib_resources tf2onnx onnxruntime onnx opencv-python wget gradio
 else
   echo "Setting up regular set of packages"
-  python3 -m pip install "tensorflow[and-cuda]" tensorflow-model-optimization tf_keras numpy numba tensorboard tensorboard-plugin-profile etils importlib_resources tf2onnx onnxruntime onnx opencv-python wget gradio
+  #python3 -m pip install nvidia-cudnn-cu12 tensorflow==2.17.0 tensorflow-model-optimization tf_keras numpy numba tensorboard tensorboard-plugin-profile tf2onnx onnxruntime onnx opencv-python wget gradio
+  #This will probably install keras 3.5+ which currently has a problam with the dataloader so revert back to a working version
+  #python3 -m pip install keras==3.4.0
 
-#python3 -m pip install nvidia-cudnn-cu12 tensorflow==2.17.0 tensorflow-model-optimization tf_keras numpy numba tensorboard tensorboard-plugin-profile tf2onnx onnxruntime onnx opencv-python wget gradio
-#This will probably install keras 3.5+ which currently has a problam with the dataloader so revert back to a working version
-#python3 -m pip install keras==3.4.0
+  #For a 50XX series card
+  #wget https://github.com/mypapit/tensorflowRTX50/releases/download/2.20dev-ubuntu-24.04-avx-too/tensorflow-2.20.0dev0+selfbuild-cp312-cp312-linux_x86_64.whl
+  #python3 -m pip install numpy numba  etils importlib_resources opencv-python wget gradio 
+  #python3 -m pip install tensorflow-2.20.0dev0+selfbuild-cp312-cp312-linux_x86_64.whl
 
-#For a 50XX series card
-#wget https://github.com/mypapit/tensorflowRTX50/releases/download/2.20dev-ubuntu-24.04-avx-too/tensorflow-2.20.0dev0+selfbuild-cp312-cp312-linux_x86_64.whl
-#python3 -m pip install numpy numba  etils importlib_resources opencv-python wget gradio 
-#python3 -m pip install tensorflow-2.20.0dev0+selfbuild-cp312-cp312-linux_x86_64.whl
-#This just installs latest version (use it with care)
-python3 -m pip install tensorflow[and-cuda] tensorflow-model-optimization tf_keras numpy numba tensorboard tensorboard-plugin-profile etils importlib_resources tf2onnx onnxruntime onnx opencv-python wget gradio 
+  #This just installs latest version (use it with care)
+  python3 -m pip install tensorflow[and-cuda] tensorflow-model-optimization tf_keras numpy numba tensorboard tensorboard-plugin-profile etils importlib_resources tf2onnx onnxruntime onnx opencv-python wget gradio 
 
-#python3 -m pip install tf-nightly[and-cuda] tensorflow-model-optimization tf_keras numpy numba tensorboard tensorboard-plugin-profile etils importlib_resources tf2onnx onnxruntime onnx opencv-python wget gradio 
+  #python3 -m pip install tf-nightly[and-cuda] tensorflow-model-optimization tf_keras numpy numba tensorboard tensorboard-plugin-profile etils importlib_resources tf2onnx onnxruntime onnx opencv-python wget gradio 
 
-#sudo apt install nvidia-cudnn-cu12 
-#tensorflow 
+  #sudo apt install nvidia-cudnn-cu12 
 fi
 
 if [ -d 2d_pose_estimation/ ]; then
