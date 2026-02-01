5d2f1f84db3d98fc6859eada0e4d9893a196da13
Ammar Qammaz <ammarkov@gmail.com>
2026-02-01 15:26:22 +0200

    ...

--- scripts/downloadPretrained.sh
+++ scripts/downloadPretrained.sh
@@ -4,16 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 cd ..
 
-
 #V180 is the ICRA 26 model!
 scripts/downloadModel.sh 180
-
-
-
-#DLNAME="2d_pose_estimation_v180.zip"
-
-#rm $DLNAME
-#wget http://ammar.gr/ymapnet/archive/$DLNAME
-#unzip -o $DLNAME
 
 exit 0
