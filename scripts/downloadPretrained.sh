7dba2abeee63d6c72261088aa42d9c12b6aafa9f
Ammar Qammaz <ammarkov@gmail.com>
2026-02-25 15:27:12 +0200

    second google doc attempt..

--- scripts/downloadPretrained.sh
+++ scripts/downloadPretrained.sh
@@ -8,19 +8,16 @@ cd ..
 #Download from Google Drive for Max Speed..
 FILEID="1DPSYH3_l2T_iaTAjpkMc9oh1VOwEJdpc"
 FILENAME="model.zip"   # change to whatever you want
-
-# first request — get confirm token
-wget --quiet --save-cookies cookies.txt --keep-session-cookies \
-     "https://docs.google.com/uc?export=download&id=${FILEID}" -O- \
-  | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' > confirm.txt
-
-# second request — use confirm token to download
-wget --load-cookies cookies.txt \
-     "https://docs.google.com/uc?export=download&confirm=$(<confirm.txt)&id=${FILEID}" \
-     -O "${FILENAME}"
-
+ 
+#curl -L -c cookies.txt "https://docs.google.com/uc?export=download&id=${FILEID}" -o page.html
+#CONFIRM=$(sed -n 's/.*confirm=\([0-9A-Za-z_]*\).*/\1/p' /tmp/page.html | head -n 1)
+#curl -L -b cookies.txt "https://docs.google.com/uc?export=download&confirm=${CONFIRM}&id=${FILEID}" -o "${OUT}"
+#rm -f cookies.txt /tmp/page.html
 # clean up
-rm -f cookies.txt confirm.txt
+#rm -f cookies.txt page.html
+
+
+wget -o "$FILENAME" "drive.google.com/u/3/uc?id=$FILEID&export=download&confirm=yes"
 
 unzip -o "$FILENAME"
 
